00eb21bba1d8556d50c97303d5ec9beaef7a27b0
Rakan Almutairi <44053941+Rakan-Almutiri@users.noreply.github.com>
2019-09-26 10:38:36 +0300

    Initial commit

---
+++ app/src/main/java/almutairi/eid/rakan/com/popular_movies_part1/MainActivity.java
@@ -0,0 +1,14 @@
+package almutairi.eid.rakan.com.popular_movies_part1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
